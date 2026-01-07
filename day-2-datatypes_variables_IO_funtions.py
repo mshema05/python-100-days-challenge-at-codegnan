843a89bf292e6fc774bbc5c501c35da4f2e5e805
mshema05 <mshema029@gmail.com>
2026-01-07 17:04:08 +0530

    Add files via upload

---
+++ day-2-datatypes_variables_IO_funtions.py
@@ -0,0 +1,15 @@
+#Data types, variables and IO funtions
+name = "m s hema chowdary" #string type
+age = 59#integer type
+weight = 20#float data type
+list1 = [1,2,3,1,2,'hi',2.4,(10,20)] # list of items
+tuple1 = (10,20,30,'welcome','hello')#tuple of items
+cards = {'A',2,3,4,5,6,7,9,10,'J','K',2,3,5}# pack or card in set datatype
+student_marks={"maths":90, "telugu":85,"english":75} # dictnory datatype
+
+print("my name is:",name)
+print("my age is :",age)
+print("list of items:",list1)
+print("tuple of items:",tuple1)
+print("pack of card value:",cards)
+print("student subject wise marks:",student_marks)
