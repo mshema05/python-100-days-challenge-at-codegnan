004f9690af2537dee692727b250712af266baeb1
mshema05 <mshema029@gmail.com>
2026-01-08 16:52:51 +0530

    Add files via upload

---
+++ Day-3 simplecalculator.py
@@ -0,0 +1,13 @@
+# simple calculator operations
+# variable and operators
+a= 10
+b= 20
+addition = a+b
+print ("sum of two numbers is :",addition)
+print("subtraction of two numbers is :",a-b)
+print("ultiplication of two numbers is:",a*b)
+print("division of two numbers is:",b/a)
+print("floor division two numbers:",b//a)
+print("modulo division of two numbers:",b%a)
+print("power of two numbers:",b**a)
+      
